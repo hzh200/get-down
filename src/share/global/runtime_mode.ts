3c66b9961393fe754e558ee55f0fbb33a31905d3
bravehzh <bravehzh@gmail.com>
2023-07-14 22:30:32 +0800

    Fix dev mode running error.

--- src/share/global/runtime_mode.ts
+++ src/share/global/runtime_mode.ts
@@ -1,6 +1,6 @@
 let isDev: boolean
 
-const NODE_DEV: string | undefined = process.env.NODE_DEV
+const NODE_DEV: string | undefined = process.env.NODE_ENV
 if (NODE_DEV === 'development') {
     isDev = true
 } else if (NODE_DEV === 'production') {
